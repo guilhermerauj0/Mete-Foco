70c6d4fb25720f34cb37e6d8c0f63b8c68d76956
arthu <arthurguilhermesb@gmail.com>
2021-05-27 10:03:35 -0300

    Night mode deleted

--- app/src/main/java/com/coldrosemob/myapplication/views/MainActivity.java
+++ app/src/main/java/com/coldrosemob/myapplication/views/MainActivity.java
@@ -32,8 +32,6 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO);
-
         db = new DBHelper(this);
 
         // informações da tarefa para o banco de dados
